9cb8111189a34e4c969d459ee5064d7412003f6b
Aaron Carlson <acarlson90@gmail.com>
2018-12-09 20:30:41 -0800

    Changes to Deploy to Heroku

--- web/src/main/java/com/aaroncarlson/WebMain.java
+++ web/src/main/java/com/aaroncarlson/WebMain.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 
 @Slf4j
+@Controller
 @SpringBootApplication
 public class WebMain {
 
